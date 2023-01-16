336977f36776730ac09fb0e4e7aee730a35a9525
profCodey <otunaiyademilade@gmail.com>
2023-01-16 15:16:20 +0100

    setup all mvc folders

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-
-
 const express = require("express");
 const app = express();
-
 const createError = require("http-errors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const userRoute = require("./routes/user");
 
 
 
@@ -20,6 +18,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/users", userRoute);
 
 
 
