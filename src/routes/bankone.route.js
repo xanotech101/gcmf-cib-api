ef01d8b113644dd08df0aca30222974d009faf42
profCodey <otunaiyademilade@gmail.com>
2023-03-03 08:28:19 +0100

    bank one

--- src/routes/bankone.route.js
+++ src/routes/bankone.route.js
@@ -18,6 +18,7 @@ router.get("/history", adminAuth, getTransactionHistory);
 router.get("/statement/:account", adminAuth, getAccountStatement);
 router.post("/name-enquiry", adminAuth, getNameEnquiry);
 router.get("/transactions/:account", adminAuth, getTransactionsPaginated);
+router.get("/transfer/interbank", adminAuth, getTransactionsPaginated);
 
 
 module.exports = router;
