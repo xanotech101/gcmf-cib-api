354d3a118db9a814664fb1dded07a690f0e628e1
Adenuga <oluwatunmiseadenuga@gmail.com>
2023-04-13 06:24:10 +0100

    update ticket endpoint

---
+++ src/routes/ticket.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllTickets,
+  getMyTickets,
+  createTicket,
+  replyTicket,
+  getMyOrganizationTickets,
+  getSingleTicket
+} = require("../controller/ticket/ticket.controller.js");
+
+const { superUserAuth, allUsersAuth } = require("../middleware/auth");
+
+router.get("/all", superUserAuth, getAllTickets);
+router.get("/mine", allUsersAuth, getMyTickets);
+router.post("/create", allUsersAuth, createTicket);
+router.post("/reply/:id", superUserAuth, replyTicket);
+router.get("/organization", allUsersAuth, getMyOrganizationTickets);
+router.get("/:id", allUsersAuth, getSingleTicket);
+
+module.exports = router;
