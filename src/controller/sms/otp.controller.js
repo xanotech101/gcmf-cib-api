582ae9844a0964b9b2a96e62783cd815297778d0
Watchdog <53454418+watchDOGGGG@users.noreply.github.com>
2023-05-01 09:26:41 +0100

    created controller for batch authorizatioon

--- src/controller/sms/otp.controller.js
+++ src/controller/sms/otp.controller.js
@@ -3,6 +3,9 @@ const crypto = require("crypto");
 const User = require("../../model/user.model");
 const { sendEmail } = require("../../utils/emailService");
 const Otp = require("../../model/otp.model");
+const initiateRequestModel = require("../../model/initiateRequest.model");
+const uuid = require("uuid");
+const { default: mongoose } = require("mongoose");
 
 const generateOTP = async (req, res) => {
   try {
@@ -69,7 +72,102 @@ const generateOTP = async (req, res) => {
 };
 
 
+const generateOtpForBatchUpload = async (req, res) => {
+  try {
+    const id = req.user;
+    const user = await User.findById(id)
+    const transactionIds = req.body.transaction
+
+    const otp = crypto.randomBytes(3).toString("hex");
+    const smsBody = `Your Bulk verification code is ${otp}.`;
+
+    let num = `${user.phone}`;
+    if (num.startsWith("0")) {
+      num = num.replace("0", "+234");
+    }
+
+    let newOtp = {}
+
+    const checkForTransactionIds = await initiateRequestModel.find({ $or:[{_id: { $in: transactionIds }, status: "pending"},{_id: { $in: transactionIds }, status: "in progress"} ]})
+    if (checkForTransactionIds.length !== transactionIds.length) {
+      return res.status(400).send({ message: 'Some of these transactions do not exist or are not pending.' })
+    }
+
+    let batchVerificationIDs = []
+    let mismatch = false
+    for (let i = 0; i < checkForTransactionIds.length; i++) {
+      const batchVerificationID = checkForTransactionIds[i].batchVerificationID
+      if (!batchVerificationID) {
+        mismatch = true
+        break
+      }
+      batchVerificationIDs.push(batchVerificationID)
+    }
+
+    let newBatchverificationID = null
+    if (mismatch || [...new Set(batchVerificationIDs)].length > 1) {
+      newBatchverificationID = new mongoose.Types.ObjectId(); // generate new 5 digit batchverification ID
+      const updateCollection = await initiateRequestModel.updateMany(
+        { _id: { $in: transactionIds } },
+        { $set: { batchVerificationID: newBatchverificationID } }
+      )
+      if (!updateCollection.modifiedCount > 0) {
+        return res.status(400).send({ message: 'Error updating batchverification ID' })
+      }
+    } else {
+      newBatchverificationID = batchVerificationIDs[0]
+    }
+
+    const otpRecord = await Otp.findOne({
+      user: user._id,
+      transaction: newBatchverificationID,
+      context: req.body.context,
+    });
+
+    if (!otpRecord) {
+      // create one and send to the user
+      newOtp = new Otp({
+        user: user._id,
+        transaction: newBatchverificationID,
+        context: req.body.context,
+        otp,
+      });
+    } else {
+      newOtp = otpRecord;
+      newOtp.otp = otp;
+    }
+
+    await newOtp.save();
+
+    await sendSMS(num, smsBody);
+
+    const subject = "Verification Code";
+    const messageData = {
+      firstName: user.firstName,
+      otp: otp,
+      message: 'Your OTP is ready',
+      year: new Date().getFullYear()
+    }
+
+    await sendEmail(user.email, subject, 'otp', messageData);
+
+    return res.status(200).json({
+      message: "Successfully sent otp code",
+      data: otp,
+      status: "success",
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message,
+      data: null,
+      status: "failed",
+    });
+  }
+}
+
 
 module.exports = {
-  generateOTP
+  generateOTP, generateOtpForBatchUpload
 };
--- src/routes/otp.route.js
+++ src/routes/otp.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { generateOTP } = require("../controller/sms/otp.controller");
+const { generateOTP, generateOtpForBatchUpload } = require("../controller/sms/otp.controller");
 const { allUsersAuth } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/generate", allUsersAuth, generateOTP);
+router.post("/generateBatchOtp", allUsersAuth, generateOtpForBatchUpload);
 router.post("/regenerate", allUsersAuth, generateOTP);
+
 
 module.exports = router;
