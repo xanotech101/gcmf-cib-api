a4a450fde5f63eb72975fb6a4fd44f3cb2a94e04
Watchdog <53454418+watchDOGGGG@users.noreply.github.com>
2023-05-31 12:45:18 +0100

    added delete user

--- src/controller/user/user.controller.js
+++ src/controller/user/user.controller.js
@@ -489,6 +489,48 @@ const enableAccount = async (req, res) =>{
     })
   }
 }
+
+const DeleteAccount = async (req, res) => {
+  try {
+
+    const findAccount = await User.findOne({ _id: mongoose.Types.ObjectId(req.params.id) });
+
+    if (!findAccount) {
+      return res.status(400).send({
+        success: false,
+        message: 'This account does not exist'
+      });
+    }
+
+    const deleteUser = await User.deleteOne({ _id: req.params.id });
+
+    if (deleteUser.deletedCount < 1) {
+      return res.status(500).send({
+        success: false,
+        message: 'something went wrong error deleting user'
+      })
+    }
+    const checkForAdmin = await Account.find({ adminID: req.params.id });
+    if (checkForAdmin.length > 0) {
+      await Account.updateMany(
+        { adminID: req.params.id },
+        { $unset: { adminID: "" } }
+      );
+    }
+    return res.status(200).send({
+      success: true,
+      message: 'user successfully deleted'
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      messsage: error.message,
+    });
+  }
+}
+
+
 module.exports = {
   getOrganizationUsers,
   getUserProfile,
@@ -502,5 +544,6 @@ module.exports = {
   getUserProfileById,
   getAllAdmins,
   disableAccount,
-  enableAccount
+  enableAccount,
+  DeleteAccount
 };
