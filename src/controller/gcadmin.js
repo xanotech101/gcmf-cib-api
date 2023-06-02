70759401430ca7a14b7033f22d5be9fe2df16d64
Watchdog <53454418+watchDOGGGG@users.noreply.github.com>
2023-06-02 16:48:01 +0100

    added pagination, search and get all gc accounts

--- src/controller/gcadmin.js
+++ src/controller/gcadmin.js
@@ -4,48 +4,6 @@ const organization = require("../model/organization");
 const userModel = require("../model/user.model");
 const { getMonthName } = require("./external/externalcontroller");
 
-// async function getAllusersTiedToGCAccount(req, res) {
-//     try {
-
-//         //get gc organizationlabel
-//         const requestlabel = await organization.findOne({ label: 'Grooming Centre' })
-//         if (!requestlabel) {
-//             return res.status(400).send({
-//                 success: false,
-//                 message: 'no organization with that label'
-//             })
-//         }
-//         //get all accounts tied to the gc organization lable
-//         const request_accounts = await Account.find({
-//             organizationLabel: requestlabel._id
-//         })
-//         if (!request_accounts || request_accounts.length === 0) {
-//             return res.status(400).send({
-//                 success: false,
-//                 message: 'No account registered with this organization yet',
-//             });
-//         }
-
-//         const request_users = [];
-//         for (const account of request_accounts) {
-//             const users = await userModel.find({ organizationId: account._id });
-//             request_users.push(...users);
-//         }
-
-//         return res.status(200).send({
-//             success: false,
-//             message: 'users fetched successufully',
-//             data: request_users
-//         });
-
-
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({ message: error.message });
-//     }
-// }
-
-// Define pagination parameters
 
 async function getAllusersTiedToGCAccount(req, res) {
     const page = parseInt(req.query.page) || 1; // Current page (default: 1)
@@ -119,36 +77,6 @@ async function getAllusersTiedToGCAccount(req, res) {
     }
 }
 
-
-// async function getAllusersTiedToAnAccount(req, res) {
-//     try {
-
-//         const page = parseInt(req.query.page) || 1;
-//         const limit = 10; // Set the desired number of users per page
-
-//         const totalCount = await userModel.countDocuments({ organizationId: req.params.account });
-
-//         const totalPages = Math.ceil(totalCount / limit);
-
-//         const skip = (page - 1) * limit;
-
-//         const users = await userModel
-//             .find({ organizationId: req.params.account })
-//             .skip(skip)
-//             .limit(limit);
-
-//         return res.status(200).json({
-//             success: true,
-//             message: 'Users tied to this account',
-//             data: users,
-//             currentPage: page,
-//             totalPages: totalPages
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({ message: error.message });
-//     }
-// }
 
 async function getAllusersTiedToAnAccount(req, res) {
     try {
@@ -244,4 +172,49 @@ async function getGcAnalytics(req, res) {
         return res.status(500).json({ message: error.message });
     }
 }
-module.exports = { getAllusersTiedToGCAccount, getAllusersTiedToAnAccount, getGcAnalytics }
+
+const getAllAccountsByGCLabel = async (req, res) => {
+    try {
+      const PAGE_SIZE = parseInt(req.query.perPage) || 10; // Number of accounts per page
+  
+      const page = parseInt(req.query.page) || 1;
+      const skip = (page - 1) * PAGE_SIZE;
+  
+      const organizationLabel = await organization.findOne({ label: 'Grooming Centre' });
+      if (!organizationLabel) {
+        return res.status(400).send({
+          success: false,
+          message: 'No GC organization found',
+        });
+      }
+
+      // Count total number of accounts based on organizationLabel
+      const totalAccounts = await Account.countDocuments({ organizationLabel: organizationLabel._id });
+  
+      // Find accounts based on organizationLabel with pagination and population of admin and organizationLabel fields
+      const accounts = await Account.find({ organizationLabel: organizationLabel._id })
+        .skip(skip)
+        .limit(PAGE_SIZE)
+        .sort({ _id: -1 })
+        .populate("adminID")
+        .populate("organizationLabel");
+  
+      // Return the accounts along with additional information
+      return res.status(200).json({
+        success: true,
+        message: 'Accounts fetched successfully',
+        accounts,
+        totalAccounts,
+        currentPage: page,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+  
+  
+module.exports = { getAllusersTiedToGCAccount, getAllusersTiedToAnAccount, getGcAnalytics,getAllAccountsByGCLabel }
