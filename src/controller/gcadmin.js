4d01246197b43d94446c606615b7136ff0f31f3e
Watchdog <53454418+watchDOGGGG@users.noreply.github.com>
2023-07-06 11:16:23 +0100

    fix date conversion for query filter on analytics

--- src/controller/gcadmin.js
+++ src/controller/gcadmin.js
@@ -335,53 +335,54 @@ async function getGcAnalytics(req, res) {
       ]);
     };
 
-    const getDisbursementsPerYear = async () => {
-      return InitiateRequest.aggregate([
-        {
-          $match: {
-            organizationLabel: requestLabel._id,
-            transferStatus: "successful",
-            createdAt: {
-              $gte: new Date(year, 0, 1), // January 1st of the requested year
-              $lte: new Date(year, 11, 31), // December 31st of the requested year
-            },
-          },
-        },
-        {
-          $group: {
-            _id: {
-              month: { $month: "$createdAt" },
-            },
-            amount: { $sum: "$amount" },
-          },
-        },
-        {
-          $project: {
-            _id: 0,
-            month: {
-              $switch: {
-                branches: [
-                  { case: { $eq: ["$_id.month", 1] }, then: "January" },
-                  { case: { $eq: ["$_id.month", 2] }, then: "February" },
-                  { case: { $eq: ["$_id.month", 3] }, then: "March" },
-                  { case: { $eq: ["$_id.month", 4] }, then: "April" },
-                  { case: { $eq: ["$_id.month", 5] }, then: "May" },
-                  { case: { $eq: ["$_id.month", 6] }, then: "June" },
-                  { case: { $eq: ["$_id.month", 7] }, then: "July" },
-                  { case: { $eq: ["$_id.month", 8] }, then: "August" },
-                  { case: { $eq: ["$_id.month", 9] }, then: "September" },
-                  { case: { $eq: ["$_id.month", 10] }, then: "October" },
-                  { case: { $eq: ["$_id.month", 11] }, then: "November" },
-                  { case: { $eq: ["$_id.month", 12] }, then: "December" },
-                ],
-                default: "Invalid Month",
-              },
-            },
-            amount: 1,
-          },
-        },
-      ]);
-    };
+   const getDisbursementsPerYear = () => InitiateRequest.aggregate([
+			{
+				$match: {
+          organizationLabel:requestLabel._id,
+					transferStatus: "successful",
+					$expr: {
+						$and: [
+							{ $gte: [{ $toInt: { $substr: ["$createdAt", 0, 4] } }, parseInt(year)] },
+							{ $lte: [{ $toInt: { $substr: ["$createdAt", 0, 4] } }, parseInt(year) + 1] }
+						]
+					}
+				},
+			},
+			{
+				$group: {
+					_id: {
+						month: { $month: { $toDate: "$createdAt" } },
+					},
+					amount: { $sum: "$amount" },
+				},
+			},
+			{
+				$project: {
+					_id: 0,
+					month: {
+						$switch: {
+							branches: [
+								{ case: { $eq: ["$_id.month", 1] }, then: "January" },
+								{ case: { $eq: ["$_id.month", 2] }, then: "February" },
+								{ case: { $eq: ["$_id.month", 3] }, then: "March" },
+								{ case: { $eq: ["$_id.month", 4] }, then: "April" },
+								{ case: { $eq: ["$_id.month", 5] }, then: "May" },
+								{ case: { $eq: ["$_id.month", 6] }, then: "June" },
+								{ case: { $eq: ["$_id.month", 7] }, then: "July" },
+								{ case: { $eq: ["$_id.month", 8] }, then: "August" },
+								{ case: { $eq: ["$_id.month", 9] }, then: "September" },
+								{ case: { $eq: ["$_id.month", 10] }, then: "October" },
+								{ case: { $eq: ["$_id.month", 11] }, then: "November" },
+								{ case: { $eq: ["$_id.month", 12] }, then: "December" },
+							],
+							default: "Invalid Month",
+						},
+					},
+					amount: 1,
+				},
+			},
+		]);
+
 
     const getDisbursementsTotal = async () => {
       return InitiateRequest.aggregate([
