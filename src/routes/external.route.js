2df506f2e557d24861117ecd4f3b092ff8bef2ac
Watchdog <53454418+watchDOGGGG@users.noreply.github.com>
2024-05-05 18:14:32 +0100

    added organization key

--- src/routes/external.route.js
+++ src/routes/external.route.js
@@ -1,11 +1,13 @@
 const express = require("express")
-const { generateUserToken, getAllThirdPartyOrganizations, getthirdpartyAnalytics, initiateRequest, getAllTransactionByThirdParty } = require("../controller/external/externalcontroller")
-const { validateThirdPartyAuthorization } = require("../middleware/auth")
+const { generateUserToken, getAllThirdPartyOrganizations, getthirdpartyAnalytics, initiateRequest, getAllTransactionByThirdParty, createExternalOrganization, updateOrganizationKey } = require("../controller/external/externalcontroller")
+const { validateThirdPartyAuthorization, superUserAuth } = require("../middleware/auth")
 const { getNameEnquiry, bvnEnquiry, IntrabankAccountEnquiry, getTransactionHistory } = require("../controller/bankone/bankDetails")
 
 const externalRoute = express.Router()
 
+externalRoute.post('/createExternalOrganization', createExternalOrganization)
 externalRoute.post('/requestToken', generateUserToken)
+externalRoute.patch('/update_key', superUserAuth, updateOrganizationKey)
 externalRoute.get('/requestNameEnquiry', validateThirdPartyAuthorization, getNameEnquiry)
 externalRoute.get('/bvnValidation', validateThirdPartyAuthorization, bvnEnquiry)
 externalRoute.get('/getthirdpartyOrganization', getAllThirdPartyOrganizations)
