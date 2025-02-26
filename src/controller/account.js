a4a4be1a45a8ccfd6d936c907e2856a7d90a3e63
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-02-26 18:40:05 +0100

    chore: fixes

--- src/controller/account.js
+++ src/controller/account.js
@@ -18,7 +18,7 @@ const Organization = require("../model/organization");
 
 
 function isValidEmail(email) {
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailPattern.test(email);
 }
 
